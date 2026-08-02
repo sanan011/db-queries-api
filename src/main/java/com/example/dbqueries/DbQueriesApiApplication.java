11a39e9bc3533f0e1818f6b7b807450cd1f962fa
sanan011 <syaqubov236@gmail.com>
2026-08-03 00:07:16 +0400

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/example/dbqueries/DbQueriesApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbqueries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbQueriesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbQueriesApiApplication.class, args);
+    }
+
+}
